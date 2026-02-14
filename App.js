072efabc7b646a5d7b112bbc62a83203174c18a3
Alina Klishyna <alinakotova102@gmail.com>
2026-02-14 19:56:10 +0100

    [be] feat: Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView } from 'react-native';
+import { Button } from 'react-native';
 
 export default function App() {
+const handleTextPress = () => {  console.log("Text pressed");
+}
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text numberOfLines={1} style={styles.text} onPress={handleTextPress}>Hello!{'\n'}This is my first React Native app!</Text>
+      <Button title="Click Me" onPress={() => console.log("Button pressed")} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +19,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+   text: {
+    color: 'blue',
+    fontSize: 20,
   },
 });
